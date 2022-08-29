6d7aa77ec189cef3671ed9a4e4d3b6d2a0cec6bc
HashiDaveR <106760442+hashidave@users.noreply.github.com>
2022-08-29 11:09:21 -0500

    Update terraform.tf

--- terraform-gcp/terraform.tf
+++ terraform-gcp/terraform.tf
@@ -8,16 +8,13 @@ terraform {
       source  = "hashicorp/google"
       version = "3.5.0"
     }
+    module "network_vpc" {
+      source  = "terraform-google-modules/network/google//modules/vpc"
+      version = "5.2.0"
+    }
   }
   
-  module "network_vpc" {
-    source  = "terraform-google-modules/network/google//modules/vpc"
-    version = "5.2.0"
-    description "This is my private vpc module"
-    network_name="example-vpc"
-    project_id={var.gpc_project}
-  }
-
+  
   cloud {
     organization = "hashi-DaveR"
     hostname     = "app.terraform.io"
