8d17742da6e13f14b2b8d32b7001e3868fe6f10e
david randolph <david.randolph@hashicorp.com>
2023-01-10 12:41:10 -0600

    turned cat2 ip back on it output

--- terraform-aws/outputs.tf
+++ terraform-aws/outputs.tf
@@ -24,9 +24,9 @@ output "catapp_ip" {
 #  value = "http://" + {aws_eip.hashicat2.*.public_dns}
 #}
 
-#output "catapp_ip2" {
-#  value = "http://${aws_eip.hashicat2.*.public_ip}"
-#}
+output "catapp_ip2" {
+  value = "http://${aws_eip.hashicat2.*.public_ip}"
+}
 
 #output "catapp_spot_url" {
 #  value = "http://${aws_eip.hashicat_spot.*.public_dns}"
