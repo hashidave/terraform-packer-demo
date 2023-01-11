7e90046f016fb67a0e9ab78c0f35f2b7dc3fc3ca
david randolph <david.randolph@hashicorp.com>
2023-01-11 16:59:29 -0600

    Initial rev

---
+++ terraform-aws/boundary.tf
@@ -0,0 +1,41 @@
+# Get the project ID from the main boundary project
+data "tfe_outputs" "Boundary" {
+  organization = "hashi-DaveR"
+  workspace = "Boundary-Environment"
+}
+
+
+#resource "boundary_host_catalog_plugin" "host_catalogd" {
+#  name            = "GoldenImage AWS Dev Catalog"
+#  description     = ""
+#  scope_id        = tfe_outputs.Boundary.host_catalog
+#  plugin_name     = "aws"
+#  attributes_json = jsonencode({ 
+#	"region" = "us-east-2"
+#	"disable_credential_rotation"=true
+# 
+#  })
+ 
+#  # recommended to pass in aws secrets using a file() or using environment variables
+  # the secrets below must be generated in aws by creating a aws iam user with programmatic access
+#  secrets_json = jsonencode({
+#    "access_key_id"     = var.AWS_ACCESS_KEY_BOUNDARY_USER
+#    "secret_access_key" = var.AWS_SECRET_KEY_BOUNDARY_USER
+#  })
+#}
+
+resource "boundary_host_set_plugin" "host_set" {
+  name            = "GoldenImage AWS Dev Host Set"
+  host_catalog_id = tfe_outputs.Boundary.host_catalog
+  attributes_json = jsonencode({ "filters" = "tag:host-set=DMR_GOLDEN_IMAGE_AWS_DEV" })
+
+  # Have to set the endpoints to whatever IP Addresses that AWS asssigns
+  preferred_endpoints=["cidr:{aws_eip.hashicat.*.public_ip}/32"]
+
+}
+
+
+
+
+
+
