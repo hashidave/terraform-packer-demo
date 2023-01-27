938f1baed417a3d129d8f3b0be07e6df15e57d89
david randolph <david.randolph@hashicorp.com>
2023-01-27 15:34:15 -0600

    Added lots of good logic to start creating the dynamic secrets roles, etc.

--- terraform-rds/vault.tf
+++ terraform-rds/vault.tf
@@ -1,9 +1,55 @@
 # Vault configuration
+resource "vault_mount" "db" {
+  path = "postgres-{$var.prefix}-${var.environment}"
+  type = "database"
+}
 
 # Create a DB Connection
+resource "vault_database_secret_backend_connection" "postgres" {
+  backend       = vault_mount.db.path
+  name          = "postgres"
+  allowed_roles = ["rw", "ro"]
 
+  postgresql {
+    connection_url = "postgres://dmradmin:${random_password.pg-password.result}@${aws_db_instance.db-instance.address}:${aws_db_instance.db-instance.port}/database}"
+  }
+}
 
 # Create a read-write role
+resource "vault_database_secret_backend_role" "rw-role" {
+  backend             = vault_mount.db.path
+  name                = "rw"
+  db_name             = vault_database_secret_backend_connection.postgres.name
+  creation_statements = ["CREATE ROLE '{{name}}' WITH LOGIN PASSWORD '{{password}}' VALID UNTIL '{{expiration}}';",
+                         "GRANT pg_write_all_data TO '{{name}}';"
+                        ]
+  default_ttl         = 3600
+}
 
 
-# create a read-only role
+# Create a read-only role
+resource "vault_database_secret_backend_role" "role" {
+  backend             = vault_mount.db.path
+  name                = "ro"
+  db_name             = vault_database_secret_backend_connection.postgres.name
+  creation_statements = ["CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD '{{password}}' VALID UNTIL '{{expiration}}';",
+                         "GRANT pg_read_all_data to '{{name}}';"
+                        ]
+  default_ttl         = 3600
+}
+
+# set up roles so that boundary can generate secrets
+resource "vault_policy" "read-write" {
+  name = "read-postgres-${var.prefix}-${var.environment}"
+
+  policy = <<EOT
+path "postgres-{$var.prefix}-${var.environment}/ro" {
+  capabilities = ["read"]
+}
+path "postgres-{$var.prefix}-${var.environment}/rw" {
+  capabilities = ["read"]
+}
+
+EOT
+}
+
