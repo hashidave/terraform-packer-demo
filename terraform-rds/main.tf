70a4cf4a2b3bc76ebbf8d0a3e234bce35a00659b
david randolph <david.randolph@hashicorp.com>
2023-01-27 16:53:14 -0600

    Added security rule to allow inbound pg connections

--- terraform-rds/main.tf
+++ terraform-rds/main.tf
@@ -95,12 +95,12 @@ resource "aws_security_group" "BoundaryRDS" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  #ingress {
-  #  from_port   = 80
-  #  to_port     = 80
-  #  protocol    = "tcp"
-  #  cidr_blocks = ["0.0.0.0/0"]
-  #}
+  ingress {
+    from_port   = 5432 
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   #ingress {
   #  from_port   = 443
