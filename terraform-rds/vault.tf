e9b1d7608dbe2bd4e9d3c30f659ca1cd89528aa0
david randolph <david.randolph@hashicorp.com>
2023-01-30 10:22:09 -0600

    typo

--- terraform-rds/vault.tf
+++ terraform-rds/vault.tf
@@ -9,7 +9,7 @@ resource "vault_database_secret_backend_connection" "postgres" {
   allowed_roles = ["rw", "ro"]
 
   postgresql {
-    connection_url = "postgres://dmradmin:${random_password.pg-password.result}@${aws_db_instance.db-instance.address}:${aws_db_instance.db-instance.port}/postgres}"
+    connection_url = "postgres://dmradmin:${random_password.pg-password.result}@${aws_db_instance.db-instance.address}:${aws_db_instance.db-instance.port}/postgres"
   }
 }
 
