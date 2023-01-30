a25cb978522a06531df996dc09b2c34f08690882
david randolph <david.randolph@hashicorp.com>
2023-01-30 10:57:14 -0600

    added count index to the name of the roles

--- terraform-rds/vault.tf
+++ terraform-rds/vault.tf
@@ -6,7 +6,7 @@ resource "vault_database_secret_backend_connection" "postgres" {
   #backend       = "database/postgres-${var.prefix}-${var.environment}"
   backend       = "database"
   name          = "postgres-${var.prefix}-${var.environment}-${count.index}"
-  allowed_roles = ["rw", "ro"]
+  allowed_roles = ["rw-${count.index}", "ro-${count.index}"]
 
   postgresql {
     connection_url = "postgres://dmradmin:${random_password.pg-password.result}@${aws_db_instance.db-instance.address}:${aws_db_instance.db-instance.port}/postgres"
@@ -18,7 +18,7 @@ resource "vault_database_secret_backend_role" "rw-role" {
   count               = var.db-count
 # backend             = "database/postgres-${var.prefix}-${var.environment}"
   backend             = "database"
-  name                = "rw"
+  name                = "rw-${count.index}"
   db_name             = vault_database_secret_backend_connection.postgres[count.index].name
   creation_statements = ["CREATE ROLE '{{name}}' WITH LOGIN PASSWORD '{{password}}' VALID UNTIL '{{expiration}}';",
                          "GRANT pg_write_all_data TO '{{name}}';"
@@ -32,7 +32,7 @@ resource "vault_database_secret_backend_role" "role" {
   count= var.db-count
 #  backend             = "database/postgres-${var.prefix}-${var.environment}" 
   backend             = "database"
-  name                = "ro"
+  name                = "ro-${count.index}"
   db_name             = vault_database_secret_backend_connection.postgres[count.index].name
   creation_statements = ["CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD '{{password}}' VALID UNTIL '{{expiration}}';",
                          "GRANT pg_read_all_data to '{{name}}';"
