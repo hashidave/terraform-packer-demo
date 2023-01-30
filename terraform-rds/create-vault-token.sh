f6f61d4f1a8c7a9c145cd2335675eafe91596f15
david randolph <david.randolph@hashicorp.com>
2023-01-30 12:55:06 -0600

    don't need to generate a token externally.  TF will generate it on the fly

--- terraform-rds/create-vault-token.sh
+++ terraform-rds/create-vault-token.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "This is the token to hand off to boundary in the BOUNDARY_VAULT_TOKEN var"
-vault token create   -no-default-policy=true   -policy="boundary-controller"  -orphan=true   -period=168h   -renewable=true   -field=token
+#echo "This is the token to hand off to boundary in the BOUNDARY_VAULT_TOKEN var"
+#vault token create   -no-default-policy=true   -policy="general-token-policy"  -orphan=true   -period=168h   -renewable=true   -field=token
 
 echo "This is the token to give to terraform in the VAULT_TOKEN env var"
 vault token create   -no-default-policy=true   -policy="general-token-policy" -policy="create-db-mount" -orphan=true   -period=168h   -renewable=true   -field=token
