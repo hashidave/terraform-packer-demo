ccbaaa8f58c5341525abd2d3b8fd56635ba63acb
david randolph <david.randolph@hashicorp.com>
2023-02-13 17:11:45 -0600

    now gives you the commands to remote to the targets

--- terraform-aws/outputs.tf
+++ terraform-aws/outputs.tf
@@ -1,63 +1,48 @@
-output "ssh-target"{
-  value=boundary_target.server-ssh.id
-}
 
-output "ssh-brokered-target"{
-  value=boundary_target.server-ssh-brokered.id
-}
-
-output "ubuntu_iteration" {
-  value = data.hcp_packer_iteration.ubuntu
-}
-
-output "ubuntu_us_east_2" {
-  value = data.hcp_packer_image.ubuntu_us_east_2
-}
-
-output "catapp_url" { 
-  value = aws_eip.hashicat.*.public_dns
-}
-
-output "controller_token" { 
+output "boundary_controller_token" { 
   value =boundary_worker.private-worker.controller_generated_activation_token
 }
 
-#output "ssh_private_key"{
-#  value=var.ssh_private_key
-#  sensitive=true
-#}
 
+output "boundary_injected_connection_commands"{
+  value=formatlist ("boundary connect ssh -target-id=%s", boundary_target.server-ssh.id )
+}
+
+output "boundary_brokered_connection_commands"{
+  value=formatlist ("boundary connect ssh -target-id=%s", boundary_target.server-ssh-brokered.id)
+}
 
 output "boundary_worker_ec2_ip" {
   value = aws_eip.boundary-worker.public_ip
 }
 
-output "hcp_boundary_worker_ip" {
+output "boundary_worker_ip" {
   value = boundary_worker.private-worker.address
 }
-
-
-
 
 output "catapp_ip" {
   value = aws_eip.hashicat.*.public_ip
 }
 
-#output "catapp_url2" {
-#  value = "http://" + {aws_eip.hashicat2.*.public_dns}
-#}
-
 output "catapp_ip2" {
   value = aws_eip.hashicat2.*.public_ip
 }
 
-#output "catapp_spot_url" {
-#  value = "http://${aws_eip.hashicat_spot.*.public_dns}"
-#}
+output "catapp_spot_ip" {
+  value = aws_eip.hashicat_spot.*.public_ip
+}
 
-#output "catapp_spot_ip" {
-#  value = "http://${aws_eip.hashicat_spot.*.public_ip}"
-#}
+output "catapp_url" { 
+  value = flatten(formatlist("http://%s", aws_eip.hashicat.*.public_dns))
+}
+output "catapp_url2" {
+  value = flatten (formatlist("http://%s", aws_eip.hashicat2.*.public_dns))
+}
+
+output "catapp_spot_url" {
+  value = flatten(formatlist("http://%s", aws_eip.hashicat_spot.*.public_dns))
+}
+
 
 
 
@@ -65,4 +50,3 @@ output "catapp_ip2" {
 #output "test"{
 #  value=formatlist("cidr:%s/32", aws_eip.hashicat.*.public_ip)
 #}
-
