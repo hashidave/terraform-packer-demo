34ef7278001ec32dbe14cded55e2d825d4968010
david randolph <david.randolph@hashicorp.com>
2023-02-17 10:56:33 -0600

    don't need this anymore. TF is generating the token now

--- terraform-rds/create-vault-token.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "This is the token to give to terraform in the VAULT_TOKEN env var"
-vault token create   -no-default-policy=true   -policy="general-token-policy" -policy="tf-create-token" -policy="terraform-demos" -orphan=true   -period=168h   -renewable=true   -field=token 
