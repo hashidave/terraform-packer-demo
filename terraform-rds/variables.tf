4d952c0bca5b97c233656f76fcc0890c4d90f3a9
david randolph <david.randolph@hashicorp.com>
2023-03-06 12:10:13 -0600

    Removed unused vars

--- terraform-rds/variables.tf
+++ terraform-rds/variables.tf
@@ -42,10 +42,6 @@ variable "TFC_VAULT_NAMESPACE"{
   default= ""
 }
 
-variable "vault_db_mount"{
-  default= "rds-demo-db"
-}
-
 variable "address_space" {
   description = "The address space that is used by the virtual network. You can supply more than one address space. Changing this forces a new resource to be created."
   default     = "10.0.0.0/16"
