d64e422a6949d49baaf82905bd17027f13a0b860
david randolph <david.randolph@hashicorp.com>
2023-04-21 13:57:27 -0500

    Initial rev.  probably not working

---
+++ terraform-gcp/boundary.tf
@@ -0,0 +1,253 @@
+##############################################################
+####   Get the project ID from the main boundary project  ####
+##############################################################
+data "tfe_outputs" "Boundary" {
+  organization = "hashi-DaveR"
+  workspace = "Boundary-Environment-${var.environment}"
+}
+
+
+##########################################################################
+####  Provision a dynamic host set into an existing AWS catalog      ##### 
+####  that was created by the main boundary project referenced above #####
+##########################################################################
+resource "boundary_host_set_plugin" "host_set" {
+  name            = "${var.prefix} ${var.environment} Host Set"
+ 
+  # The host catalog comes from an external state for our general boundary environment
+  host_catalog_id = data.tfe_outputs.Boundary.nonsensitive_values.host_catalog
+  attributes_json = jsonencode({ "filters" = "tag:host-set=${var.prefix}_AWS_${var.environment}" })
+
+  # for public endpoints, Have to set the endpoints to whatever IP Addresses that AWS asssigns
+  # not doing that in this environment right now.
+  #preferred_endpoints=["cidr:${var.subnet_prefix}"]
+
+}
+
+
+#######################################
+############ Credential Info ##########
+#######################################
+resource "boundary_credential_store_vault" "vault-store" {
+  name        = "vault-store-${var.prefix}-${var.environment}"
+  description = "Demo connection to my HCP Vault for the ${var.prefix} ${var.environment} environment"
+  address     = var.VAULT_ADDR
+  token       = vault_token.boundary_vault_token.client_token
+  scope_id    = data.tfe_outputs.Boundary.nonsensitive_values.demo-project-id
+  namespace   = var.TFC_VAULT_NAMESPACE
+}
+
+
+### Cred library for injected creds
+
+resource "boundary_credential_library_vault" "vault-library" {
+  name                = "hcp-vault-library-${var.prefix}{-${var.environment}"
+  description         = "HCP Vault credential library for ${var.environment}"
+  credential_store_id = boundary_credential_store_vault.vault-store.id
+  credential_type     = "ssh_private_key"
+  path                = vault_kv_secret_v2.userdata.path
+  http_method         = "GET"
+}
+
+### Cred library for brokered creds
+### It's the same path as the library above but it will pull out the 
+### username/password combination instead
+resource "boundary_credential_library_vault" "vault-library-brokered" {
+  name                = "hcp-vault-library-brokered ${var.prefix} ${var.environment}"
+  description         = "HCP Vault credential library for brokered static creds for the ${var.prefix} ${var.environment} environment"
+  credential_store_id = boundary_credential_store_vault.vault-store.id
+  credential_type     = "username_password"
+  path                = vault_kv_secret_v2.userdata.path
+  http_method         = "GET"
+}
+
+
+
+####################################
+######  The targets   ##############
+####################################
+resource "boundary_target" "server-ssh" {
+  name         = "${var.prefix}-${var.environment}-server-ssh"
+  description  = "ssh target with injected creds for the ${var.prefix} ${var.environment} environment"
+  type         = "ssh"
+  default_port = "22"
+  scope_id     = data.tfe_outputs.Boundary.nonsensitive_values.demo-project-id 
+  host_source_ids = [
+    boundary_host_set_plugin.host_set.id
+  ]
+  
+  injected_application_credential_source_ids = [
+    boundary_credential_library_vault.vault-library.id
+  
+  ]
+  
+  ingress_worker_filter="\"${var.prefix}\" in \"/tags/project\" and \"${var.environment}\" in \"/tags/env\""
+}
+
+
+resource "boundary_target" "server-ssh-brokered" {
+  name         = "${var.prefix}-${var.environment}-server-ssh-brokered"
+  description  = "ssh target with brokered creds for the ${var.prefix} ${var.environment} environment"
+  type         = "ssh"
+  default_port = "22"
+  scope_id     = data.tfe_outputs.Boundary.nonsensitive_values.demo-project-id 
+
+  host_source_ids = [
+    boundary_host_set_plugin.host_set.id
+  ]
+
+  brokered_credential_source_ids = [
+     boundary_credential_library_vault.vault-library-brokered.id
+  ]
+
+  ingress_worker_filter="\"${var.prefix}\" in \"/tags/project\" and \"${var.environment}\" in \"/tags/env\""
+
+}
+
+#################################################################################################################
+####  The self-managed worker                                                                               #####
+# This is non-trivial to deploy.  First you need an HCP boundary-worker object                              ##### 
+# which will generate a token that has to be passed into the ec2 instance and injected into a config file   #####
+# Currently this is being done with a remote-exec provisioner but plan is to                                #####   
+# use AWS user-data when the ec2 is provisioned to pass that along.                                         ##### 
+# ###############################################################################################################
+
+# Create a controller-lead HCP Boundary Worker Object
+resource "boundary-worker" "private-worker"{
+  scope_id    = "global" 
+  description = "Golden Image Workflow Worker for the ${var.prefix} ${var.environment} environment"
+  name        = "${var.prefix}-${var.environment}-worker"
+
+  # The activation token on the HCP side is only available on the apply that creates the boundary-worker objevct
+  # so if we later change the worker ec2 instance for any reason we have to re-create the HCP boundary-worker
+  # so that we can get the token back.  I supppose we could store it in Vault but that's a task for future Dave.
+  lifecycle{
+     replace_triggered_by=[google_compute_instance.boundary-worker]
+  }
+}
+
+
+# Deploy a boundary worker EC2 into our environment
+  data "hcp_packer_iteration" "boundary-worker" {
+    bucket_name = "boundary-workers"
+    channel     = var.environment 
+  } 
+ 
+  data "hcp_packer_image" "boundary-worker" {
+    bucket_name    = "boundary-workers"
+    cloud_provider = "gce"
+    iteration_id   = data.hcp_packer_iteration.boundary-worker.ulid
+    region         = var.zone
+  }
+ 
+
+resource "google_compute_instance" "boundary-worker" {
+  name         = "${var.prefix}-${var.environment}-boundary-worker"
+  hostname     = "${var.prefix}-${var.environment}-boundary-worker"
+  project      = var.project_id
+  zone         = var.zone
+  machine_type = var.vm_type
+  
+  #NOTE:  Tags have to be lower case!!
+  tags = ["boundary-worker"]
+  
+  
+  #metadata = {
+  #  ssh-keys = "${var.admin}:${file("id_rsa.pub")}"
+  #}
+
+  network_interface {
+    network            = "${module.terraform_vpc.network_self_link}"
+    subnetwork         = google_compute_subnetwork.terraform_sub.self_link
+    subnetwork_project = var.project_id
+    network_ip         = var.boundary-worker_ip
+
+    access_config {
+      // Include this section to give the VM an external ip address
+    }
+  }
+  #---------------------------------------------------------------------------------------
+  # Computer Image
+  #---------------------------------------------------------------------------------------
+  boot_disk {
+    initialize_params {
+      image = data.hcp_packer_image.ubuntu_gcp.cloud_image_id
+    }
+  }
+  # scratch_disk {
+  #  interface = "SCSI"
+  #}
+
+  scheduling {
+    on_host_maintenance = "MIGRATE"
+    automatic_restart   = true
+  }
+
+
+  # service account
+  service_account {
+    scopes = ["https://www.googleapis.com/auth/compute.readonly"]
+  }
+
+}
+
+  
+  # This exist only to get around the circular dependency between the worker & the aws_eip
+  # that we have to put into the config file.
+  # should update in the future to use some kind of user-data structure that gets passed in
+  # when the cloud provisions the instance
+  resource "null_resource" "worker-provisioner" {
+    triggers={
+      worker-id=google_compute_instance.boundary-worker.id
+      #worker-ip=aws_eip.boundary-worker.public_ip
+    }
+       
+
+    connection {
+       type     = "ssh"
+       user     = "ubuntu"
+       host     = google_compute_instance.boundary-worker.network_interface.0.access_config.0.nat_ip
+       port     = 22
+       private_key = var.ssh_private_key
+     }
+
+
+     #Provisioner will fill out the stub config file with some important 
+     #Boundary info & restart the boundary-worker service
+     provisioner "remote-exec" {
+       inline=[
+         "sudo sed -i ''s/CLUSTER_ID_HERE/${var.boundary-cluster-id}/g'' /etc/boundary.d/boundary.hcl",
+	
+         "sudo sed -i ''s/CONTROLLER_GENERATED_TOKEN_HERE/${boundary-worker.private-worker.controller_generated_activation_token}/g'' /etc/boundary.d/boundary.hcl",
+         
+         "sudo sed -i ''s/WORKER_PUBLIC_IP_HERE/${google_compute_instance.boundary-worker.network_interface.0.access_config.0.nat_ip}/g'' /etc/boundary.d/boundary.hcl",
+
+         "sudo sed -i ''s/ENVIRONMENT_TAG_HERE/${var.environment}/g'' /etc/boundary.d/boundary.hcl",
+
+         "sudo sed -i ''s/PROJECT_TAG_HERE/${var.prefix}/g'' /etc/boundary.d/boundary.hcl",
+
+	       "sudo service boundary --full-restart"
+       ]
+     }
+ }
+
+
+
+#---------------------------------------------------------------------------------------
+# Firewall
+#---------------------------------------------------------------------------------------
+resource "google_compute_firewall" "boundary-worker" {
+  project     = var.project_id
+  name        = "allow-boundary-rule"
+  network     = "${module.terraform_vpc.network_name}"
+  description = "Creates firewall rule targeting tagged instances"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "9202"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["boundary-worker"]
+  timeouts {}
+}
+
---
+++ terraform-gcp/vault.tf
@@ -0,0 +1,95 @@
+### Create a couple of policies that will be applied to the token we're 
+### about to create. This token will be handed off to boundary for it's 
+### credential stores
+resource "vault_policy" "boundary_token_policy"{
+  name = "boundary-token-policy"
+  
+  policy= <<EOT
+      path "auth/token/lookup-self" {
+        capabilities = ["read"]
+      }
+      path "auth/token/renew-self" {
+        capabilities = ["update"]
+      }
+      path "auth/token/revoke-self" {
+        capabilities = ["update"]
+      }
+      path "auth/token/create" {
+        capabilities = ["create", "read", "update", "list"]
+      }
+      path "sys/leases/renew" {
+        capabilities = ["update"]
+      }
+      path "sys/leases/revoke" {
+        capabilities = ["update"]
+      }
+      path "sys/capabilities-self" {
+        capabilities = ["update"]
+      }
+    EOT  
+}
+
+resource "vault_policy" "boundary_credential_policy" {
+  name = "boundary-credential-policy"
+  policy= <<EOT
+      path "kv/data/userdata" {
+        capabilities = ["read"]
+      }
+
+      #path "kv/data/GoldenImage-UserPW-dev" {
+      #  capabilities = ["read"]
+      #}
+  EOT 
+}
+
+
+# Create a vault token to hand off to boundary
+resource "vault_token" "boundary_vault_token"{
+  period            = "168h"
+  no_default_policy = false
+  no_parent         = true
+  #give it all the policies that we created above plus the general one
+  policies= ["boundary-token-policy","boundary-credential-policy"]
+}
+
+#### Provision some default secrets into vault
+
+# first we need to mount kv
+resource "vault_mount" "kv" {
+  path      = "kv"
+  type      = "kv-v2"
+  #options   = { version = "2" }
+}
+
+# Generate a random password for our user
+resource "random_password" "user-password" {
+  length           = 16
+  special          = true
+  override_special = "+_-"
+}
+
+# have to introduce a delay between creating the kv2 mount & trying 
+# to write a secret to it.
+resource "time_sleep" "wait_30_seconds" {
+  depends_on = [vault_mount.kv]
+  create_duration = "30s"
+}
+
+resource "vault_kv_secret_v2" "userdata" {
+  mount                      = vault_mount.kv.path
+  name                       = "userdata"
+  cas                        = 1
+  delete_all_versions        = true
+  data_json                  = jsonencode(
+  {
+    private_key = var.ssh_private_key,
+    username    = var.admin,
+    password    = random_password.user-password.result
+  }
+  )
+  custom_metadata {
+    max_versions = 5
+  }
+  # gotta wait a bit..
+  depends_on = [time_sleep.wait_30_seconds]
+}
